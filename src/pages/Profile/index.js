c28176e81b3cb131616f8ed1a62960c885549014
venu <yedukondalunaik12345@gmail.com>
2024-10-30 16:42:13 +0530

    Added the profile page layout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,96 +1,65 @@
 import React, { useState } from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
+import "./profile.css";
 import {
   BottomNavigation,
   BottomNavigationAction,
-  Container,
   List,
   ListItem,
-  ListItemButton,
-  ListItemText,
 } from "@mui/material";
 import {
-  FaHome,
-  FaUserPlus,
-  FaImage,
-  FaLock,
-  FaUserEdit,
   FaCalendar,
-  FaUsers,
-  FaBuilding,
+  FaImage,
   FaUser,
+  FaUsers,
+  FaLayerGroup,
 } from "react-icons/fa";
-import { styled } from "@mui/system";
+
+const links = [
+  { path: "/user-profile", label: "Profile", icon: <FaUser /> },
+  { path: "/user-profile/add-event", label: "Events", icon: <FaCalendar /> },
+  {
+    path: "/user-profile/add-banner-images",
+    label: "Banner",
+    icon: <FaImage />,
+  },
+  { path: "/user-profile/add-members", label: "Members", icon: <FaUsers /> },
+  {
+    path: "/user-profile/add-units",
+    label: "Units",
+    icon: <FaLayerGroup />,
+  },
+];
 
 const ProfilePage = () => {
   const location = useLocation();
   const [activeTab, setActiveTab] = useState(location.pathname);
-  const links = [
-    {
-      path: "/user-profile/add-event",
-      label: "Events",
-      icon: <FaCalendar />,
-    },
-    {
-      path: "/user-profile/add-banner-images",
-      label: "Banner",
-      icon: <FaImage />,
-    },
-    { path: "/user-profile", label: "Profile", icon: <FaUser /> },
-    {
-      path: "/user-profile/add-members",
-      label: "Members",
-      icon: <FaUsers />,
-    },
-    {
-      path: "/user-profile/add-units",
-      label: "Units",
-      icon: <FaBuilding />,
-    },
-  ];
-
-  const StyledListItemButton = styled(ListItemButton)(({ active }) => ({
-    color: active ? "#d62828" : "#0077b6",
-    fontWeight: active ? "bold" : "normal",
-    transition: "color 0.3s ease",
-    padding: "10px 20px",
-    borderRadius: "8px",
-    "&:hover": {
-      color: active ? "#d62828" : "#023e8a",
-      backgroundColor: "rgba(255, 255, 255, 0.1)",
-    },
-  }));
 
   return (
     <div className="container-fluid vh-100 d-flex flex-column flex-md-row p-0">
       {/* Sidebar for larger screens */}
-      <div
-        className="col-md-3 d-none d-md-block p-3"
-        style={{
-          background: "rgba(255, 255, 255, 0.2)",
-          backdropFilter: "blur(10px)",
-          borderRight: "1px solid rgba(255, 255, 255, 0.2)",
-          borderRadius: "10px",
-          boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-          height: "100vh",
-        }}
-      >
-        <h4
-          className="text-center text-uppercase mb-4"
-          style={{ color: "#0077b6" }}
-        >
-          Profile Navigation
-        </h4>
-        <List component="nav">
+      <div className="col-md-3 d-none d-md-block h-100">
+        <List component="nav" className="list-group  gap-2 m-3">
           {links.map((link) => (
-            <ListItem key={link.path} disablePadding>
-              <StyledListItemButton
-                component={Link}
+            <ListItem
+              key={link.path}
+              className={`list-group-item rounded-3  c-pointer  ${
+                location.pathname === link.path
+                  ? " inner-box-shadow-danger"
+                  : ""
+              }`}
+            >
+              <Link
                 to={link.path}
-                active={location.pathname === link.path ? 1 : 0}
+                className={`d-flex align-items-center text-decoration-none w-100  ff-p  py-2 ${
+                  location.pathname === link.path
+                    ? "fw-medium text-red-pale fw-semibold   "
+                    : "text-blue-light"
+                }`}
               >
-                <ListItemText primary={link.label} />
-              </StyledListItemButton>
+                {link.icon}
+                <span className="ms-2">{link.label}</span>
+              </Link>
             </ListItem>
           ))}
         </List>
@@ -98,17 +67,7 @@ const ProfilePage = () => {
 
       {/* Main content area */}
       <div className="col-md-9 p-4">
-        <div
-          style={{
-            background: "rgba(255, 255, 255, 0.6)",
-            backdropFilter: "blur(5px)",
-            borderRadius: "10px",
-            padding: "20px",
-            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-            height: "100%",
-            color: "#023047",
-          }}
-        >
+        <div className="bg-white p-4 rounded-3 shadow-sm h-100">
           <Outlet />
         </div>
       </div>
@@ -118,8 +77,7 @@ const ProfilePage = () => {
         sx={{
           display: { xs: "flex", md: "none" },
           position: "fixed",
-          bottom: -10,
-          zIndex: "10000",
+          bottom: 0,
           width: "100%",
           background: "#ffffff",
           boxShadow: "0 -4px 10px rgba(0, 0, 0, 0.1)",
@@ -138,7 +96,6 @@ const ProfilePage = () => {
             sx={{
               color: activeTab === link.path ? "#d62828" : "#0077b6",
               "& .Mui-selected": { color: "#d62828" },
-              zIndex: "10000",
             }}
           />
         ))}
